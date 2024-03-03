6124e63e7c06aa5b1b1296eda56e37ca3abd3987
Saman Pordanesh <sina.pordanesh@yahoo.com>
2024-03-03 13:16:27 -0700

    Code stylometry - feature extraction scripting
    Modifying Java code stylometry scripts:
    - accepting code, directly from the dataset, instead of the code file path
    - error handling, passing all general errors and capturing null cells
    - timing and process bar implementaiton

--- evaluator_model/features/features.py
+++ evaluator_model/features/features.py
@@ -29,56 +29,70 @@ from .syntactic import JavaKeywords
 from .syntactic import MaxDepthASTNode
 from .utils import build_mapping_to_ids
 
+from tqdm.notebook import tqdm
+from joblib import Parallel, delayed
+from typing import List, Dict
 
-def calculate_features(path: str) -> Dict:
+def calculate_features(code: str) -> Dict:
     """
     Calculates a set of features for the given source file.
 
-    :param path: path to the file
+    :param code: code to be analyzed
     :return: dictionary with features
     """
 
-    with open(path, 'r', errors='ignore') as file:
-        code = file.read()
-
-    file_length = len(code)
-    tokens = list(javalang.tokenizer.tokenize(code))
-    tree = javalang.parse.parse(code)
+    # with open(path, 'r', errors='ignore') as file:
+    #     code = file.read()
 
     features = {}
 
-    # LEXICAL FEATURES
-    features.update(WordUnigramTF.calculate(tokens))
-    features.update(NumKeyword.calculate(tokens, file_length))
-    features.update(NumTokens.calculate(tokens, file_length))
-    features.update(NumLiterals.calculate(tokens, file_length))
-    features.update(NumKeywords.calculate(tokens, file_length))
-    features.update(NumFunctions.calculate(tree, file_length))
-    features.update(NumFunctions.calculate(tree, file_length))
-    features.update(NumTernary.calculate(tree, file_length))
-    features.update(AvgLineLength.calculate(code))
-    features.update(StdDevLineLength.calculate(code))
-    features.update(AvgParams.calculate(tree))
-    features.update(StdDevNumParams.calculate(tree))
+    try:
 
-    # LAYOUT FEATURES
-    features.update(NumTabs.calculate(code))
-    features.update(NumSpaces.calculate(code))
-    features.update(NumEmptyLines.calculate(code))
-    features.update(WhiteSpaceRatio.calculate(code))
-    features.update(NewLineBeforeOpenBrace.calculate(code))
-    features.update(TabsLeadLines.calculate(code))
+        if not isinstance(code, str):
+            # Return a marker or an empty dictionary to indicate this sample cannot be processed
+            return {'error': 'Invalid code type'}
+        
+        file_length = len(code)
+        tokens = list(javalang.tokenizer.tokenize(code))
+        tree = javalang.parse.parse(code)
 
-    # SYNTACTIC FEATURES
-    features.update(MaxDepthASTNode.calculate(tree))
-    features.update(ASTNodeBigramsTF.calculate(tree))
-    features.update(ASTNodeTypesTF.calculate(tree))
-    features.update(JavaKeywords.calculate(tokens))
+        # LEXICAL FEATURES
+        features.update(WordUnigramTF.calculate(tokens))
+        features.update(NumKeyword.calculate(tokens, file_length))
+        features.update(NumTokens.calculate(tokens, file_length))
+        features.update(NumLiterals.calculate(tokens, file_length))
+        features.update(NumKeywords.calculate(tokens, file_length))
+        features.update(NumFunctions.calculate(tree, file_length))
+        features.update(NumFunctions.calculate(tree, file_length))
+        features.update(NumTernary.calculate(tree, file_length))
+        features.update(AvgLineLength.calculate(code))
+        features.update(StdDevLineLength.calculate(code))
+        features.update(AvgParams.calculate(tree))
+        features.update(StdDevNumParams.calculate(tree))
 
-    return features
+        # LAYOUT FEATURES
+        features.update(NumTabs.calculate(code))
+        features.update(NumSpaces.calculate(code))
+        features.update(NumEmptyLines.calculate(code))
+        features.update(WhiteSpaceRatio.calculate(code))
+        features.update(NewLineBeforeOpenBrace.calculate(code))
+        features.update(TabsLeadLines.calculate(code))
+
+        # SYNTACTIC FEATURES
+        features.update(MaxDepthASTNode.calculate(tree))
+        features.update(ASTNodeBigramsTF.calculate(tree))
+        features.update(ASTNodeTypesTF.calculate(tree))
+        features.update(JavaKeywords.calculate(tokens))
+
+        return features
+
+    except Exception as e:
+        # Catch any kind of exception and return a generic error marker
+        # You can optionally log the exception message or type for debugging purposes
+        return {'error': 'General error'}
 
 
-def calculate_features_for_files(files: List[str], n_jobs: int = 4) -> List[Dict]:
+def calculate_features_for_files(codes: List[str], n_jobs: int = 4) -> List[Dict]:
     """
     Calculates sets of features for the given source files.
 
@@ -87,9 +101,21 @@ def calculate_features_for_files(files: List[str], n_jobs: int = 4) -> List[Dict
     :return: list with features for each source file
     """
 
-    with Parallel(n_jobs=n_jobs) as pool:
-        features = pool(delayed(calculate_features)(path) for path in files)
+    # with Parallel(n_jobs=n_jobs) as pool:
+    #     features = pool(delayed(calculate_features)(path) for path in files)
 
+    # return features
+    
+    # Initialize a tqdm notebook progress bar
+    with tqdm(total=len(codes), desc='Calculating features') as progress_bar:
+        with Parallel(n_jobs=n_jobs) as pool:
+            features = pool(
+                delayed(calculate_features)(code, progress_bar) for code in codes
+            )
+
+    # with Parallel(n_jobs=n_jobs) as pool:
+    #     features = pool(delayed(calculate_features)(code) for code in codes)
+            
     return features
 
 
